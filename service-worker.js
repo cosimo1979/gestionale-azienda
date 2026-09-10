7fe481d6d75c93c607a0a5b6454b47ec0e953456
cosimo1979 <cosimo.dinunzio1979@gmail.com>
2026-09-10 10:48:23 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,20 @@
-const CACHE='gestionale-azienda-v5';
+const CACHE='gestionale-azienda-v5-1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (event.request.method !== 'GET') return;
-  // Per HTML/JS usiamo prima la rete: evita di servire una versione vecchia del gestionale.
   if (event.request.mode === 'navigate' || url.pathname.endsWith('/index.html')) {
-    event.respondWith(
-      fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
-    );
+    event.respondWith(fetch(event.request).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
     return;
   }
   event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
