f0df8655942eb4d6f59fc6f1a6b2956371b38e06
cosimo1979 <cosimo.dinunzio1979@gmail.com>
2026-09-10 11:18:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='gestionale-azienda-v5-2';
+const CACHE='gestionale-azienda-v5-4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
